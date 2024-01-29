8a9eed64df15d8d293ff1ca253aefa9961e628b1
Nika Avalishvili <nika.avalishvili@solveva.com>
2024-01-29 13:29:35 +0400

    first try

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -1,6 +1,17 @@
 package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
-    public static void main(String[] args) {
+    private double a;
+    private double b;
+
+    public double calculateFormula(double a, double b) {
+
+        double result = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b) - (4 * a * b / 2));
+        return result;
+    }
+
+    public void main(String[] args) {
+        // You can test your formula here if needed
+        calculateFormula(a,b);
     }
 }
